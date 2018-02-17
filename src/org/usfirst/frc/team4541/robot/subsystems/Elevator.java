aeeb55956b1d86d4abdeb26cbfe39c1e14f0a775
ljgreenhill <ljgreenhill@yahoo.com>
2018-02-17 14:31:44 -0500

    Added ElevatorHome and updated Elevator

--- src/org/usfirst/frc/team4541/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team4541/robot/subsystems/Elevator.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4541.robot.subsystems;
 
 import org.usfirst.frc.team4541.robot.OI;
+
 import org.usfirst.frc.team4541.robot.Robot;
 import org.usfirst.frc.team4541.robot.RobotMap;
 import org.usfirst.frc.team4541.robot.commands.ManualMoveElevator;
@@ -12,7 +13,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -22,11 +26,112 @@ public class Elevator extends Subsystem {
 	public WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(RobotMap.elevatorMotor);
 	private boolean maintainingPos = false; // whether the elevator is using input from controller or PID
 	
+	private PIDController pidVel;
+	private PIDController pidMotorOutput;
+	
+
+	
+	// move to constant class
+	private double P_Out = 0;
+	private double I_Out = 0;
+	private double D_Out = 0;
+	
+	private double P_Vel = 0;
+	private double I_Vel = 0;
+	private double D_Vel = 0;
+	private double F_Vel = 0;
+	
+	private double maxHeight = 85; // max height in inches
+	private double minHeight = 0; // min height in inches
+	
+	private double encoderPulsesPerInch = 100;
+	private double desiredVelocity;
+	private double desiredMotorOutput;
+	
+	DigitalInput limitSwitch;
+	
+	
+	
 	public Elevator() {
+
+	    limitSwitch = new DigitalInput(0); // change input later
+	    
 		
-		elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
+		
+		elevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 		elevatorMotor.setSensorPhase(true); /* keep sensor and motor in phase */
+		
+		pidVel = new PIDController(P_Vel, I_Vel, D_Vel, new PIDSource() {
+			PIDSourceType vel_sourceType = PIDSourceType.kDisplacement;
+			
+			@Override
+			public double pidGet() {
+				return elevatorMotor.getSelectedSensorPosition(0);
+			}
+			
+			@Override 
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				vel_sourceType = pidSource;
+			}
+			
+			@Override 
+			public PIDSourceType getPIDSourceType() {
+				return vel_sourceType;
+			}
+		  
+		},
+			
+		new PIDOutput() { 
+			@Override
+			public void pidWrite(double d) {
+				desiredVelocity = d;
+			}
+		});
+		
+		pidVel.setInputRange(minHeight*encoderPulsesPerInch, maxHeight*encoderPulsesPerInch);
+		pidVel.setSetpoint(0);
+		pidVel.setOutputRange(-100, 100);
+		pidVel.setContinuous(false);
+		pidVel.setPercentTolerance(1);
+		pidVel.disable();
+		//output of pidVel is setpoint of pidMotorOutput
+		
+		pidMotorOutput = new PIDController(P_Out, I_Out, D_Out, new PIDSource() {
+			PIDSourceType out_sourceType = PIDSourceType.kDisplacement;
+			
+			@Override
+			public double pidGet() {
+				return elevatorMotor.getSelectedSensorVelocity(0);
+			}
+			
+			@Override
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				out_sourceType = pidSource;
+			}
+			
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return out_sourceType;
+			}
+		},
+				
+	    new PIDOutput() {
+			@Override
+			public void pidWrite(double d) {
+				desiredMotorOutput = d;
+			}
+		});
+		
+		pidMotorOutput.setInputRange(-100, 100);
+		pidMotorOutput.setOutputRange(-1, 1);
+		pidMotorOutput.setContinuous(false);
+		pidMotorOutput.setPercentTolerance(1);
+		pidMotorOutput.setSetpoint(0);
+		pidMotorOutput.disable();
+		
 	}
+	
+	
 	
     public void initDefaultCommand() {
     	setDefaultCommand(new ManualMoveElevator());
@@ -34,12 +139,24 @@ public class Elevator extends Subsystem {
     public WPI_TalonSRX getElevatorMotor() {
     	return elevatorMotor;
     }
-    public void setElevatorSpeed(double speed) {
+  /*  public void setElevatorSpeed(double speed) {
     	elevatorMotor.set(ControlMode.PercentOutput, speed);
-    }
+    }*/
     
     public double getElevatorPos() {
     	return Robot.elevator.getElevatorMotor().getSelectedSensorPosition(0);
     }
+    
+    public PIDController getPIDVel() {
+    	return pidVel;
+    }
+    
+    public PIDController getPIDMotorOutput() {
+    	return pidMotorOutput;
+    }
+    
+   public DigitalInput getLimitSwitch() {
+	   return limitSwitch;
+   }
 }
 
