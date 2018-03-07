6e998de8d207d2c2e0a797be21a9574cd1d5cf35
NathanMalta <NathanMalta@10.2.14.4>
2018-03-07 15:56:35 -0500

    added intake speed command

---
+++ src/org/usfirst/frc/team4541/robot/commands/setIntakeSpeed.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team4541.robot.commands;
+
+import org.usfirst.frc.team4541.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class setIntakeSpeed extends Command {
+	double speed;
+    public setIntakeSpeed(double s) { //positive = intake, negative = eject
+    	requires(Robot.intake);
+    	speed = s;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.intake.setIntakeSpeed(speed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+}
